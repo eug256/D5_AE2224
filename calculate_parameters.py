1c7c2c7a5b2adfd0c0a7b15c5b6cdab0e363d80c
dieeneman16 <luuksebscheffer@gmail.com>
2024-03-24 00:06:31 +0100

    attempt at multithreading

--- calculate_parameters.py
+++ calculate_parameters.py
@@ -4,20 +4,20 @@ import numpy as np
 from scipy.fft import fft, fftfreq
 import csv
 import time
+import multiprocessing
+import pandas as pd
+import os
 
-def parameters(trai_input, create_csv=False, timer=False):
+def parameters(trai_start, trai_end, create_csv=False, timer=False):
     if timer == True:
         start = time.time()
     with open('settings.yml', 'r') as file:
         results = yaml.safe_load(file)
         TRADB = results['tradb']
         PRIDB = results['pridb']
-        
-    ################## INPUT: ###################
-    trai = [1,trai_input]
-    #############################################
-    trai_min = min(trai)
-    trai_max = max(trai)
+
+    trai_min = trai_start
+    trai_max = trai_end
 
     pridb = vae.io.PriDatabase(PRIDB)
     df_hits = pridb.iread_hits(query_filter=f"TRAI >= {trai_min} AND TRAI <= {trai_max}")
@@ -53,3 +53,37 @@ def parameters(trai_input, create_csv=False, timer=False):
         print(end - start)
     
     return total_data
+
+def merge_csv(trai_start, trai_end, csv_files):
+    df_csv_concat = pd.concat([pd.read_csv(file) for file in csv_files ], ignore_index=True)
+    df_csv_concat.to_csv(f"{trai_start}-{trai_end}.csv",index=False)
+    for file in csv_files:
+        os.remove(file)
+
+def run(trai_start, trai_end, create_csv=False, timer=False, number_of_threads=1):
+    if multiprocessing.cpu_count() < number_of_threads:
+        print(f"Maximum number of threads is {multiprocessing.cpu_count()}")
+        quit()
+    processes = {}
+
+    number_of_trai = int((trai_end-trai_start+1)/number_of_threads)
+    actual_start = trai_start
+    actual_end = number_of_trai
+
+    csv_files = []
+
+    for index in range(number_of_threads):
+        csv_files.append(f"{actual_start}-{actual_end}.csv")
+        processes[index] = multiprocessing.Process(target=parameters, args=(actual_start, actual_end, True, timer)) 
+        actual_start += number_of_trai
+        actual_end += number_of_trai
+    for index in range(number_of_threads):
+        processes[index].start()
+    for index in range(number_of_threads):
+        processes[index].join()
+
+    merge_csv(trai_start,trai_end,csv_files)
+    
+
+if __name__ == '__main__':
+    run(1, 1000, True, True, 4)
