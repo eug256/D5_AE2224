c5cb8e745b917038db7505aa30be8e405b32259e
SnowyGranius <vlad.stoica67@gmail.com>
2024-03-26 11:30:31 +0100

    fuckfuck

--- Data_filtering.py
+++ Data_filtering.py
@@ -49,9 +49,9 @@ def filter_dataset(trai_start, trai_end, previous_last_trai = 1):
 
     return filtered_data, i # return filtered array and last trai number in the original dataset
 
-data = filter_dataset(1, 5)
+# data = filter_dataset(1, 5)
 # new_data = np.zeros((5,7))
 
 # data = np.array(parameters(1,5)) # [amplitude, energy, rise time, counts, max amplitude in the freq spectrum, variance, counts/rise time]
 # new_data[0] = data[0]
-print(data)
+# print(data)
