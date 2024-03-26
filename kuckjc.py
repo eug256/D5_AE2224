e14cd44c66ed7f2386e8bf7dc58fe824afdf1c4c
Wybe Mijnendonckx <w.mijnendonckx@student.tudelft.nl>
2024-03-26 11:04:58 +0100

    Update kuckjc.py

--- kuckjc.py
+++ kuckjc.py
@@ -1,30 +1,49 @@
 from sklearn.cluster import DBSCAN 
 from calculate_parameters import parameters
-from numpy import unique
-from numpy import where
 from matplotlib import pyplot as plt
 import matplotlib
+from sklearn.neighbors import NearestNeighbors
+import numpy as np
+trai_start = 1
+trai_end = 100000
+df = parameters(trai_end)
 
-# Initialize the data set we'll work with
-trai_start = 100
-trai_end = 200
-df = parameters(trai_start,trai_end)
+# Initialize NearestNeighbors class
+neigh = NearestNeighbors(n_neighbors=2)
+nbrs = neigh.fit(df)
+distances, indices = nbrs.kneighbors(df)
 
+# Plotting K-distance Graph
+distances = np.sort(distances, axis=0)
+distances = distances[:,1]
+plt.figure(figsize=(20,10))
+plt.plot(distances)
+plt.title('K-distance Graph',fontsize=20)
+plt.xlabel('Data Points sorted by distance',fontsize=14)
+plt.ylabel('Epsilon',fontsize=14)
+plt.show()
 # Initialize DBSCAN with desired parameters
-dbscan_opt = DBSCAN(eps=50, min_samples=10)
+eps = 22.0  # Adjust according to your dataset
+min_samples = 3  # Adjust according to your dataset
+dbscan_opt = DBSCAN(eps=eps, min_samples=min_samples)
 
 # Fit DBSCAN to your data
 dbscan_opt.fit(df)
 
 # Extract cluster labels
 cluster_labels = dbscan_opt.labels_
-colors = ['purple', 'red', 'blue', 'green']
+n_clusters = len(set(cluster_labels)) - (1 if -1 in cluster_labels else 0)
+print(cluster_labels)
+colors = ['purple', 'red', 'blue', 'green', 'orange', 'yellow', 'brown', 'black', 'pink', 'gray', 'olive', 'cyan']
 
 plt.figure(figsize=(10, 10))
-for i in range(trai):
-    plt.scatter(df[i][4], df[i][0], c=cluster_labels[i], cmap=matplotlib.colors.ListedColormap(colors[cluster_labels[i]]))
+for i in range(trai_start - 1, trai_end):
+    if cluster_labels[i] == -1:  # Noise points
+        plt.scatter(df[i][4], df[i][0], c='black', marker='x')
+    else:
+        plt.scatter(df[i][4], df[i][0], c=cluster_labels[i], cmap=matplotlib.colors.ListedColormap(colors[cluster_labels[i]]))
 
-plt.title('DBSCAN Clustering', fontsize=20)
-plt.xlabel('Feature 1', fontsize=14)
-plt.ylabel('Feature 2', fontsize=14)
+plt.title(f'DBSCAN Clustering - {n_clusters} clusters found', fontsize=20)
+plt.xlabel('MAX freq (1/Hz)', fontsize=14)
+plt.ylabel('Ampltude (m)', fontsize=14)
 plt.show()
