9b4f724ce33a63500193b3fcfc25b3a38e4facc8
nnogueirasgarc <150817326+nnogueirasgarc@users.noreply.github.com>
2024-04-23 11:22:20 +0200

    Update calculate_parameters.py

--- calculate_parameters.py
+++ calculate_parameters.py
@@ -3,7 +3,7 @@ import vallenae as vae
 import numpy as np
 from scipy.fft import fft, fftfreq
 import csv
-def parameters(trai_start, trai_end):
+def parameters(trai_start,trai_end):
     with open('settings.yml', 'r') as file:
         results = yaml.safe_load(file)
         TRADB = results['tradb']
@@ -16,28 +16,28 @@ def parameters(trai_start, trai_end):
     trai_max = max(trai)
 
     pridb = vae.io.PriDatabase(PRIDB)
-    df_hits = pridb.iread_hits(query_filter=f"TRAI >= {trai_min} AND TRAI <= {trai_max}")
+    df_hits = new_func(trai_min, trai_max, pridb) 
     fields = ['Amplitude', 'Energy', 'Rise_time', 'Count', 'Max_freq', 'Variance'] 
     total_data = []
 
     for i in df_hits:
         with vae.io.TraDatabase(TRADB) as tradb:
             y, t = tradb.read_wave(i[12])
-            
+
+        max_amplitude = max(np.abs(y)) 
         yf = fft(y)
         dt = t[1] - t[0]
         freq = fftfreq(len(y), dt)
-        freq_0 = []
-        amplitude_spectrum_0 = []
-        amplitude_spectrum = 2*np.abs(yf)
+        freq_0 = freq[freq >= 0]
+        amplitude_spectrum = 2 * np.abs(yf)
+        amplitude_spectrum_0 = amplitude_spectrum[freq >= 0]
 
-        for j in range(len(freq)):
-            if freq[j] >= 0:
-                freq_0.append(freq[j])
-                amplitude_spectrum_0.append(amplitude_spectrum[j])
-        
-        total_data.append([i[3],i[5],i[9],i[11],freq_0[np.argmax(amplitude_spectrum_0)],round(np.var(y)*10**10,3)])
+        total_data.append([max_amplitude, i[5], i[9], i[11], freq_0[np.argmax(amplitude_spectrum_0)], round(np.var(y) * 10 ** 10, 3), i[11] / i[9]])
     return total_data
+
+def new_func(trai_min, trai_max, pridb):
+    df_hits = pridb.iread_hits(query_filter=f"TRAI >= {trai_min} AND TRAI <= {trai_max}")
+    return df_hits
 
 def parameters_filtering(trai_start,trai_end):
     with open('settings.yml', 'r') as file:
@@ -64,15 +64,11 @@ def parameters_filtering(trai_start,trai_end):
         yf = fft(y)
         dt = t[1] - t[0]
         freq = fftfreq(len(y), dt)
-        freq_0 = []
-        amplitude_spectrum_0 = []
-        amplitude_spectrum = 2*np.abs(yf)
-        for j in range(len(freq)):
-            if freq[j] >= 0:
-                freq_0.append(freq[j])
-                amplitude_spectrum_0.append(amplitude_spectrum[j])
-        
-        total_data.append([max_amplitude,i[5],i[9],i[11],freq_0[np.argmax(amplitude_spectrum_0)],round(np.var(y)*10**10,3),i[11]/i[9]])
+        freq = fftfreq(len(y), dt)
+        freq_0 = freq[freq >= 0]
+        amplitude_spectrum = 2 * np.abs(yf)
+        amplitude_spectrum_0 = amplitude_spectrum[freq >= 0]
+        total_data.append([max_amplitude, i[5], i[9], i[11], freq_0[np.argmax(amplitude_spectrum_0)], round(np.var(y) * 10 ** 10, 3), i[11] / i[9]])
     return total_data
 
 def calculate_stuff(trai_start,trai_end):
@@ -109,4 +105,4 @@ def calculate_stuff(trai_start,trai_end):
         write.writerow(['amplitude','frequency'])
         write.writerows(total_data)
 
-calculate_stuff(1,100000)
+#calculate_stuff(1,100000)
