c3a3eb2d4a6723ea43ebc29f6aa8ec11d270f72a
nnogueirasgarc <150817326+nnogueirasgarc@users.noreply.github.com>
2024-05-02 16:58:16 +0200

    God

--- DBSCAN2 copy.py
+++ DBSCAN2 copy.py
@@ -6,25 +6,39 @@ from plotnine import *
 from sklearn.neighbors import NearestNeighbors
 from matplotlib import pyplot as plt
 import numpy as np
-
-
+import math
+distances_total = []
 HERE = os.path.dirname(__file__) if "__file__" in locals() else os.getcwd()
-DATA = os.path.join(HERE, "1-10000-filtered.csv")
+DATA = os.path.join(HERE, "5700000-5800000-filtered.csv")
 
 X = pd.read_csv(DATA) # read data
+
 #X = X.loc[(X['frequency'] >= 125000) & (X['frequency'] <= 150000)]
 X_scaled = StandardScaler().fit(X).transform(X)
+X_scaled = X_scaled[:, : 5]
+print(X_scaled)
 
-EPS=20
-SAMPLES=15
+SAMPLES=10
 
 neighbors = NearestNeighbors(n_neighbors=SAMPLES)
-neighbors_fit = neighbors.fit(X)
-distances, indices = neighbors_fit.kneighbors(X)
+neighbors_fit = neighbors.fit(X_scaled)
+distances, indices = neighbors_fit.kneighbors(X_scaled)
 
 distances = np.sort(distances, axis=0)
-distances = distances[:,1]
-plt.plot(distances)
+for i in range(len(distances)):
+    distances_total.append(np.sqrt(np.sum(distances[i,:]**2)))
+
+plt.plot(distances_total)
+plt.show()
+
+angular_variation = np.diff(distances_total, 1000)
+
+knee_index = np.argmax(angular_variation) + 2
+
+EPS = distances_total[knee_index]
+
+print("Calculated eps value:", EPS)
+
 
 db = DBSCAN(eps=EPS, min_samples=SAMPLES).fit(X_scaled)
 labels = db.labels_
