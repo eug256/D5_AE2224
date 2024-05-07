01a01e233a7c989d836466c4f5c28a0ee1bab783
JanOppelaar <90770036+JanOppelaar@users.noreply.github.com>
2024-05-07 10:43:34 +0200

    Calculated new filtered data

--- Data_filtering.py
+++ Data_filtering.py
@@ -12,8 +12,8 @@ def decide(array):
 
     # features to take into account, maybe change this later if not good
     #var = array[7]
-    counts = array[0]
-    energy = array[1]
+    counts = array[5]
+    energy = array[2]
 
     # look at counts:
     if counts >= 3 and energy >= 85.15572: # change this:
@@ -42,8 +42,6 @@ def calc_filter_data(trai_start,trai_end):
 
 
     for i in df_hits:
-        #print(i)
-        """
         with vae.io.TraDatabase(TRADB) as tradb:
             y, t = tradb.read_wave(int(i[12]))
             
@@ -57,25 +55,22 @@ def calc_filter_data(trai_start,trai_end):
             if freq[j] >= 0:
                 freq_0.append(freq[j])
                 amplitude_spectrum_0.append(amplitude_spectrum[j])
-        variance = round(np.var(y) * 10**10)
-        feature_array = [max(np.abs(y)), freq_0[np.argmax(amplitude_spectrum_0)], i[4], i[5], i[6], i[9], i[11], variance]
-        """
-        feature_array = [i[11],i[5],i[12]]
+        feature_array = [max(np.abs(y)),i[4],i[5],i[6],i[9],i[11],freq_0[np.argmax(amplitude_spectrum_0)],i[12]]
         
         if decide(feature_array) == True:
             total_data.append(feature_array)
             counter_list.append(counter)
         counter +=1
-        if counter%100000 == 0:
+        if counter%1000 == 0:
             print(counter)
 
     time1 = time.time()
     
-    with open(f'{trai_min}-{trai_max}-filtered.csv', 'w', newline='') as f:
+    with open(f'{trai_min}-{trai_max}-filtered-new.csv', 'w', newline='') as f:
     # using csv.writer method from CSV package
         write = csv.writer(f)
-        
-        write.writerow(['counts','energy','trai'])
+
+        write.writerow(['amplitude','duration','energy','rms','rise time','counts','peak frequency','trai'])
         write.writerows(total_data)
     
     print(round(time1-time0))
@@ -125,7 +120,12 @@ def calc_energy(trai_start,trai_end):
     tp = np.percentile(E,30)
     return tp
 
-calc_filter_data(1,15400000)
+calc_filter_data(5500000,5600000)
+print("one done")
+calc_filter_data(5600000,5700000)
+print("two done")
+calc_filter_data(5700000,5800000)
+print("all three done")
 
 #the thirtieth percentile of all the waves (up to 15.4 million) is 85.15572
 
